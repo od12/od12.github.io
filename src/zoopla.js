50b9eee22904af4a58ff8ea40c8f1af573239ac8
Samson Danziger <samson@samsondanziger.com>
2019-05-12 16:56:18 +0100

    Add zoopla changes

--- src/zoopla.js
+++ src/zoopla.js
@@ -1,6 +1,18 @@
-const Zoopla = require('zoopla');
-const apikeys = require('./apikeys');
-const zoopla = new Zoopla({ apiKey: apikeys.ZOOPLA });
+import * as Zoopla from 'zoopla';
+import apikeys from './apikeys';
+import { map } from './map';
+import { Overlay } from 'ol';
 
-module.exports = zoopla;
+export const zoopla = new Zoopla({ apiKey: apikeys.ZOOPLA });
+
+export function addProperties(postcode, radius) {
+  zoopla.getPropertyListings({ 
+    postcode, 
+    radius,
+    page_size: 100,
+    summarised: true,
+  }).then((response) => {
+    console.log(response);
+  }).catch((error) => { console.log(error); });
+}
 
